873331c0c26dbe25e54e7e8ef12a3bdfcdd38c98
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-06-18 21:18:35 -0400

    Add files via upload

--- kernel.asm
+++ kernel.asm
@@ -12,15 +12,30 @@ start:
 
 	mov si, text_string	; call string function. si stands for source index (look that up if you need to)
 	call print_string
+	call option_menu
 
 	jmp $			; everything loops back to here
 
 
-	text_string db 'Welcome to the OS kernel', 0
+	text_string db 'Welcome to PenisOS', 0
 
 
 print_string:
 	mov ah, 0Eh		; initialize 10h, the 'print char' function
+
+option_menu:
+	mov ax, os_version
+	mov cx, 00000100b	; color code :O
+	
+	mov ax, option_1
+	mov bx, option_2
+	
+	jmp option_menu
+
+	os_version	db 'Version 0.0.1'
+	
+	option_1	db 'option 1'
+	option_2	db 'option 2'
 
 .repeat:
 	lodsb			; get the string
